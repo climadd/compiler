314e638bf763d915489c932458b3ee49230777eb
climadd <67705396+climadd@users.noreply.github.com>
2024-09-12 13:51:17 +0200

    first commit
    scanner structure added

---
+++ src/scanner/Scanner.java
@@ -0,0 +1,79 @@
+package scanner;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+
+import token.*;
+
+public class Scanner {
+	final char EOF = (char) -1; 
+	private int riga;
+	private PushbackReader buffer;
+	private String log;
+
+	// skpChars: insieme caratteri di skip (include EOF) e inizializzazione
+	// letters: insieme lettere e inizializzazione
+	// digits: cifre e inizializzazione
+
+	// char_type_Map: mapping fra caratteri '+', '-', '*', '/', ';', '=', ';' e il
+	// TokenType corrispondente
+
+	// keyWordsMap: mapping fra le stringhe "print", "float", "int" e il
+	// TokenType  corrispondente
+
+	public Scanner(String fileName) throws FileNotFoundException {
+
+		this.buffer = new PushbackReader(new FileReader(fileName));
+		riga = 1;
+		// inizializzare campi che non hanno inizializzazione
+	}
+
+	public Token nextToken()  {
+
+		// nextChar contiene il prossimo carattere dell'input (non consumato).
+		char nextChar = peekChar(); //Catturate l'eccezione IOException e 
+		       // ritornate una LexicalException che la contiene
+
+		// Avanza nel buffer leggendo i carattere in skipChars
+		// incrementando riga se leggi '\n'.
+		// Se raggiungi la fine del file ritorna il Token EOF
+
+
+		// Se nextChar e' in letters
+		// return scanId()
+		// che legge tutte le lettere minuscole e ritorna un Token ID o
+		// il Token associato Parola Chiave (per generare i Token per le
+		// parole chiave usate l'HaskMap di corrispondenza
+
+		// Se nextChar e' o in operators oppure 
+		// ritorna il Token associato con l'operatore o il delimitatore
+
+		// Se nextChar e' in numbers
+		// return scanNumber()
+		// che legge sia un intero che un float e ritorna il Token INUM o FNUM
+		// i caratteri che leggete devono essere accumulati in una stringa
+		// che verra' assegnata al campo valore del Token
+
+		// Altrimenti il carattere NON E' UN CARATTERE LEGALE sollevate una
+		// eccezione lessicale dicendo la riga e il carattere che la hanno
+		// provocata. 
+
+
+	}
+
+	// private Token scanNumber()
+
+	// private Token scanId()
+
+	private char readChar() throws IOException {
+		return ((char) this.buffer.read());
+	}
+
+	private char peekChar() throws IOException {
+		char c = (char) buffer.read();
+		buffer.unread(c);
+		return c;
+	}
+}
---
+++ src/token/Token.java
@@ -0,0 +1,23 @@
+package token;
+
+public class Token {
+
+	private int riga;
+	private TokenType tipo;
+	private String val;
+	
+	public Token(TokenType tipo, int riga, String val) {
+	}
+	
+	public Token(TokenType tipo, int riga) {
+	}
+
+    // Getters per i campi
+    
+	public String toString() {
+		return null;
+	}
+
+     
+
+}
