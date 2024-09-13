f2a65103340e297c64e5e7027bbab0bdaaaff35a
climadd <67705396+climadd@users.noreply.github.com>
2024-09-13 22:29:04 +0200

    commit

--- src/token/Token.java
+++ src/token/Token.java
@@ -2,22 +2,37 @@ package token;
 
 public class Token {
 
-	private int riga;
-	private TokenType tipo;
-	private String val;
+	private int riga;	//riga del codice in cui si trova il token
+	private TokenType tipo;	//enum
+	private String val;	// per identificatori e numeri contiene la string matchata
 	
 	public Token(TokenType tipo, int riga, String val) {
+		this.tipo = tipo;
+		this.riga = riga;
+		this.val = val;
 	}
 	
 	public Token(TokenType tipo, int riga) {
+		this(tipo, riga, null);
 	}
 
     // Getters per i campi
     
 	public String toString() {
+		//TODO: implementa metodo
 		return null;
 	}
 
+
+	/*	esempio output toString()
+	 * 
+	 * 	<TYINT,r:1><ID,r:1,tempa><SEMI,r:1>
+		<ID,r:2,tempa><OP_ASSIGN,r:2,=><INT,r:2,5><SEMI,r:2>
+		<TYFLOAT,r:3><ID,r:3,tempb><OP_ASSIGN,r:3,=><ID,r:3,tempa><DIVIDE,r:3><FLOAT,r:3,3.2><SEMI,r:2>
+		<ID,r:4,tempb><OP_ASSIGN,r:4,+=><INT,r:4,7><SEMI,r:4>
+		<PRINT,r:5><ID,r:5,tempb><SEMI,r:5><EOF,r:5>
+
      
+     */
 
 }
