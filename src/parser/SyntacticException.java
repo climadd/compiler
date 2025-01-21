f109e889ffd3a03ddf1ea85bc4e389814f7ec9ad
climadd <67705396+climadd@users.noreply.github.com>
2025-01-21 15:27:04 +0100

    Javadoc Update on Exceptions

--- src/parser/SyntacticException.java
+++ src/parser/SyntacticException.java
@@ -2,16 +2,41 @@ package parser;
 
 import scanner.LexicalException;
 
+/**
+ * Represents the exception that occurs during syntactic analysis, 
+ * thrown when Parser's methods encounter invalid syntax while attempting
+ * to parse a stream of tokens according to the rules of the grammar.
+ */
 public class SyntacticException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+    /**
+     * Constructor of SyntacticException.
+     *
+     * @param msg information about the lexical error.
+     */
 	public SyntacticException(String msg) {
 		super(msg);
 	}
 	
-	//PER WRAPPARE LE LEXICAL EXC NEL CODICE DI PARSING :)
+	//INTERFACE FOR SYNTAX ERROR HANDLING IN PARSER
+    /**
+     * An interface needed to wrap methods that may throw a LexicalException.
+     * <p>
+     * This interface allows scanner methods to be wrapped and executed with consistent error
+     * handling.
+     *
+     * @param <T> the type of the result returned by the wrapped method.
+     */
 	public interface SyntaxErrorWrapper <T> {
+		
+	   /**
+         * Executes the wrapped method, which may throw a LexicalException.
+         *
+         * @return the result of the executed method.
+         * @throws LexicalException if a lexical error occurs.
+         */
 		  T execute() throws LexicalException;
 	}
 }
--- src/scanner/LexicalException.java
+++ src/scanner/LexicalException.java
@@ -1,9 +1,19 @@
 package scanner;
 
+/**
+ * Represents the exception that occurs during lexical analysis, 
+ * thrown when Scanner's methods encounter invalid input and 
+ * can't be tokenized properly.
+ */
 public class LexicalException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+    /**
+     * Constructor of LexicalException.
+     *
+     * @param msg information about the lexical error.
+     */
 	public LexicalException(String msg) {
 		super(msg);
 	}
